02922686f44d20ce28b271ea64fe516adf41fab4
sirinapaa <b6122775@office365.sut.ac.th>
2022-02-01 21:55:20 +0700

    add leave management

--- src/Pages/Pagenotfound.js
+++ src/Pages/Pagenotfound.js
@@ -2,17 +2,17 @@ import "./Pagenotfound.css";
 
 function Pagenotfound() {
   return (
-    <div class="box" id="container">
-      <div className="content">
-        <h2 className="pagetitle">404</h2>
-      <p>Page Not Found</p>
+    <body>
+      <div class="box" id="container">
+        <div className="content">
+          <h2 className="pagetitle">404</h2>
+          </div>
+          <p>Page Not Found</p>
 
-      <a onClick={() => (window.location = "/")}>Back to home</a>
-      
+          <a onClick={() => (window.location = "/")}>Back to home</a>
+        
       </div>
-      
-      
-    </div>
+    </body>
   );
 }
 
